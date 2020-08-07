5e5325dfd7bcd9af113e8130f42704431b3a4993
Vanderlock <valendor70@gmail.com>
2020-08-07 16:01:37 +0300

    add 14 task

---
+++ Everything Is an Object/14/Documentation2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+//: object/Documentation2.java
+/**
+* <pre>
+* System.out.println(new Date());
+* </pre>
+*/
+///:~
+public class Documentation2 {
+	Date date = new Date();
+	void showDate() {
+	 System.out.println("date = " + date);
+	}
+}
---
+++ Everything Is an Object/14/Final.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+// object/Documentation4.java
+/**
+* You can even insert a list:
+* <ol>
+* <li> Item one
+* <li> Item two
+* <li> Item three
+* </ol>
+ 
+
+		
+		* Another test list
+		* <ol>
+		* <li> 1
+		* <li> 2
+		* <li> 3
+		* </ol>
+		*/	
+
+public class Final {
+
+		/** public field list
+		* <ol>
+		* <li> 4
+		* <li> 5
+		* <li> 6
+		* </ol>
+		*/	
+		
+		public int i = 2;
+
+		/**
+		* private field list 
+		* <ol>
+		* <li> 7
+		* <li> 8
+		* <li> 9
+		* </ol>
+		*/	
+
+		private int j = 3;
+
+		/**
+		* <ol>
+		* <li> 10
+		* <li> 11
+		* <li> 12
+		* </ol><br>
+		*/	
+
+	public static void main(String[] args) {
+
+		/**
+		* another test list here
+		* <ol>
+		* <li> 13
+		* <li> 14
+		* <li> 15
+		* </ol>
+		*/	
+
+		Date date = new Date();
+		System.out.println("date = " + date);
+	}
+}
