0bea756d119b238ab9d48c5650f9f1d4ed3160f0
Vanderlock <valendor70@gmail.com>
2020-08-08 16:52:03 +0300

    Final final edit task 10 )

--- Everything Is an Object/10/Example.java
+++ Everything Is an Object/10/Example.java
@@ -1,13 +1,15 @@
- public class Example
-   {
-      public static void main(String[] args)
-      {
-   	 for ( int i = 0; i < args.length; i++ ){
-		if (i<3){
-   	    System.out.println( args[i] );
-	}else{
-		System.out.println("The result cannot be shown. The program can display only 3 command line arguments");
-		}
-	}
-      }
-   }
+public class Example{
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("There can be only 3 entered arguments.");
+	    System.out.println("You entered " +args.length + " arguments");
+        } else {
+            for(int i = 0; i < args.length; ++i) {
+
+                System.out.println(args[i]);
+
+            }
+        }
+    }
+}
