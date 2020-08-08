c8ab7c5475448906c21a86fed8d504a24a1cd28d
Vanderlock <valendor70@gmail.com>
2020-08-08 16:16:45 +0300

    added final version 10 task

--- Everything Is an Object/10/Example.java
+++ Everything Is an Object/10/Example.java
@@ -2,7 +2,12 @@
    {
       public static void main(String[] args)
       {
-   	 for ( int i = 0; i < 3; i++ )
+   	 for ( int i = 0; i < args.length; i++ ){
+		if (i<3){
    	    System.out.println( args[i] );
+	}else{
+		System.out.println("The result cannot be shown. The program can display only 3 command line arguments");
+		}
+	}
       }
    }
