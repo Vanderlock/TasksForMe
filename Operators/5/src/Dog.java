8608ea7c43bd64f58284c4bc83118580aac5e4b8
Vanderlock <valendor70@gmail.com>
2020-08-15 16:55:14 +0300

    Fix tasks 5 and 14

--- Operators/5/src/Dog.java
+++ Operators/5/src/Dog.java
@@ -1,4 +1,19 @@
 public class Dog {
-    String name;
-    String says;
+    private String name;
+    private String says;
+
+    public Dog() {
+    }
+
+    public Dog(String name, String says) {
+        this.name = name;
+        this.says = says;
+    }
+
+    @Override
+    public String toString() {
+        return name +
+                " says: "
+                + says;
+    }
 }
--- Operators/5/src/Runner.java
+++ Operators/5/src/Runner.java
@@ -1,15 +1,10 @@
 public class Runner {
     public static void main(String[] args) {
-        Dog spot = new Dog();
-        Dog scruffy = new Dog();
+        Dog spot = new Dog("Spot","Ruff!");
+        Dog scruffy = new Dog("Scruffy","Wurf!");
 
-        spot.name = "Spot";
-        spot.says = "Miau";
-        scruffy.name = "Scruffy";
-        scruffy.says = "A`m angrrrrry";
-
-        System.out.println("I`m dog " + spot.name + " and i say: " + spot.says);
-        System.out.println("I`m dog " + scruffy.name + " and i say: " + scruffy.says);
+        System.out.println(spot.toString());
+        System.out.println(scruffy.toString());
 
     }
 }
