d23b3818d3b31085536501026d1813208d2b2af0
Vanderlock <valendor70@gmail.com>
2020-10-15 12:46:27 +0300

    added converter project

---
+++ myProjects/Json to Xml Converter First/src/main/java/ConvertJSON.java
@@ -0,0 +1,105 @@
+import org.json.JSONObject;
+import org.json.XML;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import java.io.*;
+
+
+public class ConvertJSON {
+
+
+    public static void main(String[] args) {
+        final String JSON_FILE = "src\\main\\resources\\testJson.json";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(JSON_FILE));
+            FileWriter writer = new FileWriter("src\\main\\resources\\trueXML.xml", false);
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+            String ls = System.getProperty("line.separator");
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+
+            // delete the last new line separator
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            reader.close();
+            String content = stringBuilder.toString();
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + toPrettyString(convertJson(content), 5));
+            writer.flush();
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+
+    }
+
+
+    public static String convertJson(String jsonValue) {
+
+        String xml = null;
+
+        try {
+            JSONObject jsonObject = new JSONObject(jsonValue);
+
+            xml = XML.toString(jsonObject);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println();
+        }
+
+
+        return xml;
+    }
+
+    public static String toPrettyString(String xml, int indent) {
+        try {
+            // Turn xml string into a document
+            Document document = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder()
+                    .parse(new InputSource(new ByteArrayInputStream(xml.getBytes("utf-8"))));
+
+            // Remove whitespaces outside tags
+            document.normalize();
+            XPath xPath = XPathFactory.newInstance().newXPath();
+            NodeList nodeList = (NodeList) xPath.evaluate("//text()[normalize-space()='']",
+                    document,
+                    XPathConstants.NODESET);
+
+            for (int i = 0; i < nodeList.getLength(); ++i) {
+                Node node = nodeList.item(i);
+                node.getParentNode().removeChild(node);
+            }
+
+            // Setup pretty print options
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            transformerFactory.setAttribute("indent-number", indent);
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            // Return pretty print xml string
+            StringWriter stringWriter = new StringWriter();
+            transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+            return stringWriter.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
